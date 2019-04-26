cf3ff43b70096ea0260768454a1b2a2fa5300736
Eric <ericszywala@gmail.com>
2019-04-26 08:13:28 -0400

    refactor with prettier

--- src/contact.js
+++ src/contact.js
@@ -1,25 +1,22 @@
-
-
 function appendContact(parent) {
   parent.appendChild(contactTitle());
   parent.appendChild(phoneNumber());
   parent.appendChild(contactForm());
 }
 
-
 function contactTitle() {
-  let title = document.createElement('h3');
-  title.id = "contact-title"
+  let title = document.createElement("h3");
+  title.id = "contact-title";
   title.appendChild(document.createTextNode("Reach Out To Us"));
 
   return title;
 }
 
 function phoneNumber() {
-  let container = document.createElement('div');
+  let container = document.createElement("div");
   container.id = "phone";
 
-  let phoneNumber = document.createElement('h6');
+  let phoneNumber = document.createElement("h6");
   phoneNumber.appendChild(document.createTextNode("(555) 555-1234"));
   container.appendChild(phoneNumber);
 
@@ -27,36 +24,36 @@ function phoneNumber() {
 }
 
 function contactForm() {
-  let form = document.createElement('form');
-  form.setAttribute('method', 'post');
-  form.className ="contact-form";
+  let form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.className = "contact-form";
 
-  let userNameDisplay = document.createElement('h4');
-  userNameDisplay.appendChild(document.createTextNode("Name:"))
+  let userNameDisplay = document.createElement("h4");
+  userNameDisplay.appendChild(document.createTextNode("Name:"));
 
-  let userName = document.createElement('input');
+  let userName = document.createElement("input");
   userName.setAttribute("type", "text");
-  userName.setAttribute("name", "username")
+  userName.setAttribute("name", "username");
   userName.placeholder = "John Smith";
 
-  let emailDisplay = document.createElement('h4');
-  emailDisplay.appendChild(document.createTextNode("Email:"))
+  let emailDisplay = document.createElement("h4");
+  emailDisplay.appendChild(document.createTextNode("Email:"));
 
-  let email = document.createElement('input');
+  let email = document.createElement("input");
   email.setAttribute("type", "text");
-  email.setAttribute("name", "email")
+  email.setAttribute("name", "email");
   email.placeholder = "Jsmith@fake.com";
 
-  let messageDisplay = document.createElement('h4');
-  messageDisplay.appendChild(document.createTextNode("Message:"))
+  let messageDisplay = document.createElement("h4");
+  messageDisplay.appendChild(document.createTextNode("Message:"));
 
-  let message = document.createElement('textarea');
+  let message = document.createElement("textarea");
   message.setAttribute("type", "textarea");
   message.setAttribute("name", "title");
-  message.id = "message-area"
+  message.id = "message-area";
   message.placeholder = "Your message..";
 
-  let submit = document.createElement('input');
+  let submit = document.createElement("input");
   submit.setAttribute("type", "submit");
   submit.setAttribute("value", "Send your message");
 
@@ -71,5 +68,4 @@ function contactForm() {
   return form;
 }
 
-
-export {appendContact};
+export { appendContact };
--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
-import {appendFrame} from './base-frame'
-import {appendAbout} from './about'
-import {appendMenu} from './menu'
-import {appendContact} from './contact'
+import { appendFrame } from "./base-frame";
+import { appendAbout } from "./about";
+import { appendMenu } from "./menu";
+import { appendContact } from "./contact";
 
-const content = document.getElementById('content');
+const content = document.getElementById("content");
 appendFrame(content);
 
-const mainContent = document.querySelector('.main-content');
-appendAbout(mainContent)
+const mainContent = document.querySelector(".main-content");
+appendAbout(mainContent);
 
-const aboutBtn = document.querySelector('#aboutTab');
-const menuBtn = document.querySelector('#menuTab');
-const contactBtn = document.querySelector('#contactTab');
+const aboutBtn = document.querySelector("#aboutTab");
+const menuBtn = document.querySelector("#menuTab");
+const contactBtn = document.querySelector("#contactTab");
 
 const switchTabs = (() => {
-  aboutBtn.addEventListener('click',function(){manageChildren('about'), switchButtons('about')}, false);
-  menuBtn.addEventListener('click',function(){manageChildren('menu'), switchButtons('menu')}, false);
-  contactBtn.addEventListener('click',function(){manageChildren('contact'), switchButtons('contact')}, false);
+  aboutBtn.addEventListener(
+    "click",
+    function() {
+      manageChildren("about"), switchButtons("about");
+    },
+    false
+  );
+  menuBtn.addEventListener(
+    "click",
+    function() {
+      manageChildren("menu"), switchButtons("menu");
+    },
+    false
+  );
+  contactBtn.addEventListener(
+    "click",
+    function() {
+      manageChildren("contact"), switchButtons("contact");
+    },
+    false
+  );
 
   //set initial tab to about
   switchButtons("about");
@@ -27,41 +45,34 @@ const switchTabs = (() => {
   }
 
   function removeChildren() {
-    while(mainContent.firstChild) {
-        mainContent.removeChild(mainContent.firstChild);
-       }
+    while (mainContent.firstChild) {
+      mainContent.removeChild(mainContent.firstChild);
+    }
   }
 
   function addChild(child) {
-    if(child == "about") { 
-      appendAbout(mainContent); }
-    else if(child == "menu") {
+    if (child == "about") {
+      appendAbout(mainContent);
+    } else if (child == "menu") {
       appendMenu(mainContent);
-    }
-    else{
+    } else {
       appendContact(mainContent);
     }
   }
 
   function switchButtons(btn) {
     if (btn == "about") {
-      aboutBtn.style.backgroundColor= "rgb(238, 211, 188)";
-      menuBtn.style.backgroundColor= "rgb(202, 164, 134)"
-      contactBtn.style.backgroundColor= "rgb(202, 164, 134)"
-
+      aboutBtn.style.backgroundColor = "rgb(238, 211, 188)";
+      menuBtn.style.backgroundColor = "rgb(202, 164, 134)";
+      contactBtn.style.backgroundColor = "rgb(202, 164, 134)";
     } else if (btn == "menu") {
       menuBtn.style.backgroundColor = "rgb(238, 211, 188)";
-      aboutBtn.style.backgroundColor= "rgb(202, 164, 134)";
-      contactBtn.style.backgroundColor= "rgb(202, 164, 134)"
-
+      aboutBtn.style.backgroundColor = "rgb(202, 164, 134)";
+      contactBtn.style.backgroundColor = "rgb(202, 164, 134)";
     } else {
-      contactBtn.style.backgroundColor= "rgb(238, 211, 188)"
-      aboutBtn.style.backgroundColor= "rgb(202, 164, 134)";
-      menuBtn.style.backgroundColor= "rgb(202, 164, 134)"
+      contactBtn.style.backgroundColor = "rgb(238, 211, 188)";
+      aboutBtn.style.backgroundColor = "rgb(202, 164, 134)";
+      menuBtn.style.backgroundColor = "rgb(202, 164, 134)";
     }
   }
 })();
-
-
-
-
